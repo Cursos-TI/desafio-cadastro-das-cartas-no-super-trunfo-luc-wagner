84db4996d676cdf49a289ffcf50af0178f8aa441
luc-wagner <lucaswagnersoares@gmail.com>
2025-03-18 22:32:21 +0000

    Revisão do Código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,7 @@ int main() {
     float pib;
     int pontosturisticos;
     
-    printf ("Complete os dados da Carta 1: \n"); //Carta 1
+    printf ("Complete os dados da Carta 1: \n"); //Inserir os dados da Carta 1
 
     printf ("Digite o Estado: \n");
     scanf ("%s", estado);
@@ -32,13 +32,13 @@ int main() {
     printf ("Digite o número de pontos turísticos: \n");
     scanf ("%d", &pontosturisticos);
 
-    printf ("\nDados da Carta 1: \n"); //Dados da carta 1
+    printf ("\nDados da Carta 1: \n"); //Exibição dos dados da carta 1
     printf ("Estado: %s\n", estado);
     printf ("Código da Cidade: %s\nNome da Cidade: %s\n", codigodacidade, nomedacidade);
     printf ("População: %d\nÁrea: %f\n", populacao, area);
     printf ("PIB: %f\nNúmero de Pontos Turísticos: %d\n", pib, pontosturisticos);
 
-    printf ("\nComplete os dados da Carta 2: \n"); //Carta 2
+    printf ("\nComplete os dados da Carta 2: \n"); //Inserir os dados da carta 2
 
     printf ("Digite o Estado: \n");
     scanf ("%s", estado);
@@ -61,7 +61,7 @@ int main() {
     printf ("Digite o número de pontos turísticos: \n");
     scanf ("%d", &pontosturisticos);
 
-    printf ("\nDados da Carta 2: \n"); //Dados da Carta 2
+    printf ("\nDados da Carta 2: \n"); //Imprimir os dados da Carta 2
     printf ("Estado: %s\n", estado);
     printf ("Código da Cidade: %s\nNome da Cidade: %s\n", codigodacidade, nomedacidade);
     printf ("População: %d\nÁrea: %f\n", populacao, area);
